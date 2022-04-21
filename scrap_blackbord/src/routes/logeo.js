805583a68dff703deace0408a2714b57defb1199
gerli-arce <yoninfantearce@gmail.com>
2022-04-21 18:57:22 -0500

    NTS

--- scrap_blackbord/src/routes/logeo.js
+++ scrap_blackbord/src/routes/logeo.js
@@ -1,5 +1,3 @@
-
-
 async function logueo(page, credentials) {
     await page.goto('https://senati.blackboard.com/auth-saml/saml/login?apId=_242_1&redirectUrl=https%3A%2F%2Fsenati.blackboard.com%2Fultra');
 
@@ -9,15 +7,14 @@ async function logueo(page, credentials) {
     await page.waitForSelector('#i0116');
     await page.type('#i0116', credentials.user);
     await page.keyboard.press('Enter');
-
-    await page.waitForTimeout(1000);
-
     await page.waitForSelector('#i0118');
+    await page.waitForTimeout(1500);
     await page.type('#i0118', credentials.password);
+    await page.waitForTimeout(1500);
     await page.keyboard.press('Enter');
-    await page.waitForTimeout(2000);
+    //await page.waitForTimeout(2000);
+    await page.waitForSelector('.win-button.button_primary.button.ext-button.primary.ext-primary')
     await page.keyboard.press('Enter');
     return page;
 }
-
 module.exports = logueo;
