607c2bcc799eaa0342a6d5b6aa45a3d7aa3ac4d2
Gysmoy <gamboapalominocarlosmanuel@gmail.com>
2022-04-21 21:59:25 -0500

    NTS

--- scrap_blackbord/src/routes/logeo.js
+++ scrap_blackbord/src/routes/logeo.js
@@ -1,22 +1,31 @@
-
-
 async function logueo(page, credentials) {
+    var start_messages = [];
+    
     await page.goto('https://senati.blackboard.com/auth-saml/saml/login?apId=_242_1&redirectUrl=https%3A%2F%2Fsenati.blackboard.com%2Fultra');
-
-    console.log('    ⤷ Iniciando sesión en BLACKBOARD ↻');
-   // console.log(credentials)
-
-    await page.waitForSelector('#i0116');
+    await page.waitForResponse(response => {
+        return response.url().includes('signin-options');
+    });
+    start_messages.push('BlackBoard cargó correctamente');
+    start_messages.push('Ingresando usuario');
     await page.type('#i0116', credentials.user);
-    await page.keyboard.press('Enter');
+    await page.click('#idSIButton9');
 
-    await page.waitForTimeout(1000);
+    await page.waitForResponse(response => {
+        return response.url().includes('arrow_left');
+    });
 
-    await page.waitForSelector('#i0118');
+    start_messages.push('Ingresando contraseña');
     await page.type('#i0118', credentials.password);
-    await page.keyboard.press('Enter');
+    await page.click('#idSIButton9');
+    
+    start_messages.push('Confirmando caché de navegador');
     await page.waitForTimeout(2000);
-    await page.keyboard.press('Enter');
+    await page.click('#idBtn_Back');
+
+    await page.waitForResponse('https://senati.blackboard.com/learn/api/v1/streams/ultra');
+    start_messages.push('BlackBoard ultra cargó correctamente');
+
+    console.table(start_messages);
     return page;
 }
 
