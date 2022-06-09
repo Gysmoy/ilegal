3564b6b6441c8309bfb50b24da5e252bd12c9bbb
gerli-arce <yoninfantearce@gmail.com>
2022-06-08 19:44:13 -0500

    NTS

--- recoleccion_pruebas.js
+++ recoleccion_pruebas.js
@@ -17,7 +17,72 @@ $('.multiple-answer-question [ng-model="assessmentQuestion.questionAttempt.quest
 })
 console.log(questions)
 
+users = [
+    {
+        "username":"kanturin",
+        "user":"1222871@senati.pe",
+        "pass":"Pob01757"
+    },
+    {
+        "username":"ESLI VICENTE CARHUACHIN CRUZ",
+        "user":"1327130@senati.pe",
+        "pass":"Virtual20"
+    },
+    {
+        "username":"JOSE RAUL HEREDIA SOLANO",
+        "user":"1303819@senati.pe",
+        "pass":"Virtual20"
+    },
+   {
+        "username":"JIMMY ULISIS MALPARTIDA ASIS",
+        "user":"983085@senati.pe",
+        "pass":"Virtual20"
+    },
+    {
+        "username":"JENIFER SMARON LOPEZ GORA",
+        "user":"1239805@senati.pe",
+        "pass":"Simulacro19"
+    },
+    {
+        "username":"DANIEL JAIME PACHECO VARGAS",
+        "user":"1292844@senati.pe",
+        "pass":"Virtual20"
+    },
+    {
+        "username":"Aleshando",
+        "user":"1300275@senati.pe",
+        "pass":"Alex2021"
+    },
+    {
+        "username":"Yon",
+        "user":"1290272@senati.pe",
+        "pass":"asucar123YON"
+    },
+    {
+        "username":"Carlos",
+        "user":"1324182@senati.pe",
+        "pass":"14deMayoRN2018"
+    }
 
+]
+;
+users.forEach(async (user)=>{
+    var req = {};
+    req.username = user.user;
+    req.password = user.pass;
+    var res = await fetch('http://localhost:8080/qualitas/getDataBB', {
+        method: 'POST',
+        body:JSON.stringify(req),
+        headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        }
+    })
+    
+    var data = await res.json();
+    console.log(data);
+    
+});
 
 /*
 
